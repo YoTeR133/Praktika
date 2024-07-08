b8c03b8396eea18b5d8a7f0e82cbc5cdc28d2fe5
Chumenko Igor <chumenk7@gmail.com>
2024-07-08 20:06:53 +0500

    После получения файла greet_user.py из удаленного репозитория Файл был редактирован и отправлен из локального репозитория на удалённый

--- greet_user.py
+++ greet_user.py
@@ -1,12 +1,12 @@
 def greet_user():
     name = input("Введите ваше имя: ")
     if name:
-        print(f"Привет, {name}!")
+        print(f"Привет, {name}!!!")
     else:
         print("Вы не ввели имя.")
 
 def farewell_user():
-    print("До свидания!")
+    print("До свидания!!!")
 
 if __name__ == "__main__":
     greet_user()
