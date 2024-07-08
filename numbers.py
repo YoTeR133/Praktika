450cae92b67bd5a4e80f3ed743244a11eebb355c
Chumenko Igor <chumenk7@gmail.com>
2024-07-08 17:06:33 +0500

    Добавление файла numbers.py

---
+++ numbers.py
@@ -0,0 +1,10 @@
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    result = multiply(num1, num2)
+    print(f"Результат умножения: {result}")
+
+# Программа завершена.
