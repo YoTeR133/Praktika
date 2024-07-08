0f7eda14ba34b5557e0ceca22dc77dd159630bc0
Chumenko Igor <chumenk7@gmail.com>
2024-07-08 17:04:46 +0500

    Редактирование файла greet_user.py (Добавление, удаление и изменение строк)

--- greet_user.py
+++ greet_user.py
@@ -1,8 +1,13 @@
 def greet_user():
     name = input("Введите ваше имя: ")
-    print(f"Привет, {name}!")
+    if name:
+        print(f"Привет, {name}!")
+    else:
+        print("Вы не ввели имя.")
+
+def farewell_user():
+    print("До свидания!")
 
 if __name__ == "__main__":
     greet_user()
-
-print("Программа выполнена.")
+    farewell_user()
